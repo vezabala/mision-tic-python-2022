a995567b08d6cbb6280c8e5514ba5b879b0d8bd6
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-05-26 15:09:11 -0500

    chore: Ejercicio Reto 4

---
+++ Reto 4/main.py
@@ -0,0 +1,170 @@
+def listaText():
+    texto = 'En muchas partes del cuerpo como son las manos, las orejas o los pies, están representados todos los órganos y partes del cuerpo. Incidiendo sobre estas zonas se pueden crear arcos reflejos que actúen directamente sobre cualquier órgano del cuerpo y que solucionen cualgquier anomalía que exista.En las manos, las orejas o los pies, se representan todos los órganos del cuerpo. Incidiendo sobre estas zonas se pueden crear arcos reflejos que actúen directamente sobre cualquier punto del organismo y que solucionan cualquier punto del organismo y que solucionen la anomalía que exista. En muchas partes del cuerpo como son las manos, las orejas o los pies, están representados todos los órganos y partes del cuerpo. Incidiendo sobre estas zonas se pueden crear arcos reflejos que actúen directamente sobre cualquier órgano del cuerpo y que solucionen cualgquier anomalía que exista.En las manos, las orejas o los pies, se representan todos los órganos del cuerpo. Incidiendo sobre estas zonas se pueden crear arcos reflejos que actúen directamente sobre cualquier punto del organismo y que solucionan cualquier punto del organismo y que solucionen la anomalía que exista. En muchas partes del cuerpo como son las manos, las orejas o los pies, están representados todos los órganos y partes del cuerpo. Incidiendo sobre estas zonas se pueden crear arcos reflejos que actúen directamente sobre cualquier órgano del cuerpo y que solucionen cualgquier anomalía que exista.En las manos, las orejas o los pies, se representan todos los órganos del cuerpo. Incidiendo sobre estas zonas se pueden crear arcos reflejos que actúen directamente sobre cualquier punto del organismo y que solucionan cualquier punto del organismo y que solucionen la anomalía que exista. En muchas partes del cuerpo como son las manos, las orejas o los pies, están representados todos los órganos y partes del cuerpo. Incidiendo sobre estas zonas se pueden crear arcos reflejos que actúen directamente sobre cualquier órgano del cuerpo y que solucionen cualgquier anomalía que exista.En las manos, las orejas o los pies, se representan todos los órganos del cuerpo. Incidiendo sobre estas zonas se pueden crear arcos reflejos que actúen directamente sobre cualquier punto del organismo y que solucionan cualquier punto del organismo y que solucionen la anomalía que exista.'
+    lista_texto = []
+    lista_texto2 = ['Había', 'una', 'vez', 'un', 'burro', 'que', 'se', 'encontraba', 'en', 'el', 'campo', 'feliz',
+                    'comiendo', 'hierba', 'a', 'sus', 'anchas', 'y', 'paseando', 'tranquilamente', 'bajo', 'el',
+                    'cálido', 'sol', 'de', 'primavera', 'De', 'repente', 'le', 'pareció', 'ver', 'que', 'había', 'un',
+                    'lobo', 'escondido', 'entre', 'los', 'matorrales', 'con', 'cara', 'de', 'malas', 'intenciones',
+                    'Seguro', 'que', 'iba', 'a', 'por', 'él', 'Tenía', 'que', 'escapar', 'El', 'pobre', 'borrico',
+                    'sabía', 'que', 'tenía', 'pocas', 'posibilidades', 'de', 'huir', 'No', 'había', 'lugar', 'donde',
+                    'esconderse', 'y', 'si', 'echaba', 'a', 'correr', 'el', 'lobo', 'que', 'era', 'más', 'rápido', 'le',
+                    'atraparía', 'Tampoco', 'podía', 'rebuznar', 'para', 'pedir', 'auxilio', 'porque', 'estaba',
+                    'demasiado', 'lejos', 'de', 'la', 'aldea', 'y', 'nadie', 'le', 'oiría', 'Desesperado', 'comenzó',
+                    'a', 'pensar', 'en', 'una', 'solución', 'rápida', 'que', 'pudiera', 'sacarle', 'de', 'aquel',
+                    'apuro', 'El', 'lobo', 'estaba', 'cada', 'vez', 'más', 'cerca', 'y', 'no', 'le', 'quedaba', 'mucho',
+                    'tiempo', '–', 'Sí', 'eso', 'es', '–', 'pensó', 'el', 'burrito', '–', 'Fingiré', 'que', 'me', 'he',
+                    'clavado', 'una', 'espina', 'y', 'engañaré', 'al', 'lobo', 'Y', 'tal', 'como', 'se', 'le',
+                    'ocurrió', 'empezó', 'a', 'andar', 'muy', 'despacito', 'y', 'a', 'cojear', 'poniendo', 'cara', 'de',
+                    'dolor', 'y', 'emitiendo', 'pequeños', 'quejidos', 'Cuando', 'el', 'lobo', 'se', 'plantó', 'frente',
+                    'a', 'él', 'enseñando', 'los', 'colmillos', 'y', 'con', 'las', 'garras', 'en', 'alto', 'dispuesto',
+                    'a', 'atacar', 'el', 'burro', 'mantuvo', 'la', 'calma', 'y', 'siguió', 'con', 'su', 'actuación',
+                    '–', 'Ay', 'qué', 'bien', 'que', 'haya', 'aparecido', 'señor', 'lobo', 'He', 'tenido', 'un',
+                    'accidente', 'y', 'sólo', 'alguien', 'tan', 'inteligente', 'como', 'usted', 'podría', 'ayudarme',
+                    'El', 'lobo', 'se', 'sintió', 'halagado', 'y', 'bajó', 'la', 'guardia', '–', 'En', 'qué', 'puedo',
+                    'ayudarte', '–', 'dijo', 'el', 'lobo', 'creyéndose', 'sobradamente', 'preparado', '–', 'Fíjese',
+                    'qué', 'mala', 'suerte', '–', 'lloriqueó', 'el', 'burro', '–', 'Iba', 'despistado', 'y', 'me', 'he',
+                    'clavado', 'una', 'espina', 'en', 'una', 'de', 'las', 'patas', 'traseras', 'Me', 'duele', 'tanto',
+                    'que', 'no', 'puedo', 'ni', 'andar', 'Al', 'lobo', 'le', 'pareció', 'que', 'no', 'pasaba', 'nada',
+                    'por', 'echarle', 'un', 'cable', 'al', 'burro', 'Se', 'lo', 'iba', 'a', 'comer', 'de', 'todas',
+                    'maneras', 'y', 'estando', 'herido', 'no', 'podría', 'escapar', 'de', 'sus', 'fauces', '–', 'Está',
+                    'bien…', 'Veré', 'qué', 'puedo', 'hacer', 'Levanta', 'la', 'pata', 'El', 'lobo', 'se', 'colocó',
+                    'detrás', 'del', 'burro', 'y', 'se', 'agachó', 'No', 'había', 'rastro', 'de', 'la', 'astilla',
+                    'por', 'ninguna', 'parte', '–', 'No', 'veo', 'nada', '–', 'le', 'dijo', 'el', 'lobo', 'al', 'burro',
+                    '–', 'Sí', 'fíjate', 'bien…', 'Está', 'justo', 'en', 'el', 'centro', 'de', 'mi', 'pezuña', 'Ay',
+                    'cómo', 'duele', 'Acércate', 'más', 'para', 'verla', 'con', 'claridad', 'El', 'lobo', 'cayó', 'en',
+                    'la', 'trampa', 'En', 'cuanto', 'pegó', 'sus', 'ojos', 'a', 'la', 'pezuña', 'el', 'burro', 'le',
+                    'dio', 'una', 'enorme', 'coz', 'en', 'el', 'hocico', 'y', 'salió', 'pitando', 'a', 'refugiarse',
+                    'en', 'la', 'granja', 'de', 'su', 'dueño', 'El', 'lobo', 'se', 'quedó', 'malherido', 'en', 'el',
+                    'suelo', 'y', 'con', 'cinco', 'dientes', 'rotos', 'por', 'la', 'patada', 'Qué', 'estúpido', 'se',
+                    'sintió', 'Creyéndose', 'más', 'listo', 'que', 'nadie', 'fue', 'engañado', 'por', 'un', 'simple',
+                    'burro', '–', 'Me', 'lo', 'merezco', 'porque', 'sin', 'tener', 'ni', 'idea', 'me', 'lancé', 'a',
+                    'ser', 'curandero', 'Moraleja', 'cada', 'uno', 'tiene', 'que', 'dedicarse', 'a', 'lo', 'suyo', 'y',
+                    'no', 'tratar', 'de', 'hacer', 'cosas', 'que', 'no', 'sabe', 'Como', 'dice', 'el', 'refrán',
+                    'zapatero', 'a', 'tus', 'zapatos']
+    for i in texto.split():
+        lista_texto.append(i)
+    main(lista_texto2)
+
+
+def pasar_minuscula(lista):
+    lista_texto_minuscula = []
+    texto = ''
+    for i in lista:
+        texto = texto + ' ' + i
+    texto = texto.lower()
+    for j in texto.split():
+        lista_texto_minuscula.append(j)
+    return lista_texto_minuscula
+
+
+def quitar_signos(lista):
+    lista_sin_signo = []
+    lista_caracteres_especiales = ['-', '¿', '?', '.', ',', '¡', '!', ':', '"', '–', ' ']
+    condicion = 0
+    while condicion == 0:
+        contador = 0
+        palabra = ''
+        palabra2 = ''
+        lista_palabra = []
+        for i in lista:
+            palabra = palabra + i
+            break
+        for p in palabra:
+            lista_palabra.append(p)
+        contadorint = 0
+        while contadorint == 0:
+            signo = 0
+            for j in lista_caracteres_especiales:
+                for l in lista_palabra:
+                    if j == l:
+                        signo += 1
+                        lista_palabra.pop(contador)
+                    else:
+                        contador += 1
+                contador = 0
+            if signo == 0:
+                contadorint = 1
+        for pa in lista_palabra:
+            palabra2 = palabra2 + pa
+        if palabra2 != '':
+            lista_sin_signo.append(palabra2)
+        lista.pop(0)
+        if len(lista) == 0:
+            condicion = 1
+    return lista_sin_signo
+
+
+def palabras_repetidas(lista):
+    diccionario = {}
+    condicion_while = 0
+    while condicion_while == 0:
+        palabra = ''
+        contador = 0
+        for i in lista:
+            palabra = palabra + i
+            break
+        for j in lista:
+            if j == palabra:
+                contador += 1
+                diccionario[palabra] = contador
+        condicion_while2 = 0
+        while condicion_while2 == 0:
+            indice = 0
+            validacion = 0
+            for ot in lista:
+                if ot == palabra:
+                    validacion += 1
+                    lista.pop(indice)
+                    break
+                else:
+                    indice += 1
+            if validacion == 0:
+                condicion_while2 = 1
+        if len(lista) == 0:
+            condicion_while = 1
+    return diccionario
+
+
+def veinte_palabras_repetidas(diccionario):
+    diccionario_20_palabras = {}
+    numeros = []
+    contador = 0
+    for i in range(1000, 0, -1):
+        numeros.append(i)
+    while contador < 20:
+        for j in numeros:
+            for h in diccionario:
+                if j == diccionario[h]:
+                    diccionario_20_palabras[h] = diccionario[h]
+                    contador += 1
+                if contador == 20:
+                    break
+            if contador == 20:
+                break
+    return diccionario_20_palabras
+
+
+def convertir_diccionario_a_ista(diccionario):
+    lista_palabras = []
+    for i in diccionario:
+        lista_palabras.append([i, diccionario[i]])
+    return lista_palabras
+
+
+def main(lista_texto):
+    lista_texto = pasar_minuscula(lista_texto)
+    lista_texto = quitar_signos(lista_texto)
+    diccionario_todas_palabras = palabras_repetidas(lista_texto)
+    diccionario_veinte_palabras = veinte_palabras_repetidas(diccionario_todas_palabras)
+    lista_conteo = convertir_diccionario_a_ista(diccionario_veinte_palabras)
+    print(lista_conteo)
+    return lista_conteo
+
+
+if __name__ == '__main__':
+    listaText()
+    """
+    main(['hola', 'como', 'estas,', 'mo', 'hola??', 'mo', 'hola,,,.???', 'estas.,-.,'])
+    """
+    # main(['hola', 'hola?', 'hola,,', 'hola...'])
+    # main(['–','hola','bebe','sota','–'])
